0aee16fbf9d3af48e56df70c97e1eb9bc855a8ae
Vincent Zhao <gyroknight@users.noreply.github.com>
2021-02-25 07:03:17 -0500

    Xilinx DMA stuff

---
+++ tests/axidma_mem_tb.cc
@@ -0,0 +1,15 @@
+#include <systemc>
+
+#include "demo-dma.h"
+#include "iconnect.h"
+#include "xilinx-axidma.h"
+
+int sc_main(int argc, char* argv[]) {
+  axidma dma("axidma");
+  axidma_mm2s mm2s("axidma-mm2s");
+  axidma_s2mm s2mm("axidma-s2mm");
+  iconnect<5, 10> bus("bus");
+  demodma demoDma("demo-dma");
+
+  return 0;
+}
