6bcb9cc80a51cb8ba1f934475c087d3898bea134
asultan123 <asultan@aucegypt.edu>
2022-02-23 03:53:32 -0500

    factored out descriptor generation

--- src/layer_generation.cc
+++ src/layer_generation.cc
@@ -1,108 +1,109 @@
 #include "layer_generation.hh"
 
-namespace LayerGeneration{
-
-template <typename DataType>
-xt::xarray<int> generate_ifmap(Arch<DataType> &arch, int channel_in, int ifmap_h, int ifmap_w)
+namespace LayerGeneration
 {
-    auto input_size = ifmap_h * ifmap_w * channel_in;
-    assert(input_size <= arch.ifmap_mem_size);
 
-    xt::xarray<int> ifmap = xt::arange((int)1, input_size + 1);
-    ifmap.reshape({channel_in, ifmap_h, ifmap_w});
-
-    return ifmap;
-}
-
-template <typename DataType>
-xt::xarray<int> generate_weights(int filter_out_dim, int channel_in_dim, int kernel)
-{
-    int kernel_size = kernel * kernel;
-    xt::xarray<int> weights = xt::arange(1, channel_in_dim * filter_out_dim * kernel_size + 1);
-
-    weights.reshape({filter_out_dim, channel_in_dim, kernel, kernel});
-    return weights;
-}
-
-template <typename DataType>
-xt::xarray<int> pad_weights(Arch<DataType> &arch, xt::xarray<int> weights, int filter_out_dim, int channel_in_dim, int kernel, UnrollOrientation unroll_orientation)
-{
-    int kernel_size = kernel * kernel;
-    
-    vector<vector<deque<int>>> pe_weights(arch.filter_count, vector<deque<int>>(arch.channel_count, deque<int>()));
-
-    long unsigned int verticle_padding;
-    long unsigned int horizontal_padding;
-
-    switch (unroll_orientation)
+    template <typename DataType>
+    xt::xarray<int> generate_ifmap(Arch<DataType> &arch, int channel_in, int ifmap_h, int ifmap_w)
     {
-    case UnrollOrientation::HORIZONTAL:
-    {
-        weights.reshape({filter_out_dim, channel_in_dim * kernel_size});
-        verticle_padding = ceil((float)filter_out_dim / arch.filter_count) * arch.filter_count - filter_out_dim;
-        horizontal_padding = ceil((float)(channel_in_dim * kernel_size) / arch.channel_count) * arch.channel_count - (channel_in_dim * kernel_size);
-        break;
-    }
-    default:
-        cout << "INVALID ORIENTATION" << endl;
-        exit(EXIT_FAILURE);
-        break;
+        auto input_size = ifmap_h * ifmap_w * channel_in;
+        assert(input_size <= arch.ifmap_mem_size);
+
+        xt::xarray<int> ifmap = xt::arange((int)1, input_size + 1);
+        ifmap.reshape({channel_in, ifmap_h, ifmap_w});
+
+        return ifmap;
     }
 
-    xt::xarray<int> padded_weights = xt::pad(weights, {{0, verticle_padding}, {0, horizontal_padding}}, xt::pad_mode::constant, PAD);
-    
-    return padded_weights;
-}
-
-bool validate_output_1x1(xt::xarray<int> ifmap, xt::xarray<int> weights, xt::xarray<int> arch_output)
-{
-    // weights.shape() = F*C*K*K
-    assert(weights.shape().size() == 4);
-    // cout << xt::adapt(weights.shape()) << endl;
-    // ifmap.shape() = C*H*W
-    assert(ifmap.shape().size() == 3);
-    // cout << xt::adapt(ifmap.shape()) << endl;
-    // cout << ifmap << endl;
-    // symmetric kernel
-    assert(weights.shape(3) == weights.shape(2));
-
-    // ifmap channel = weights channel in
-    assert(ifmap.shape(0) == weights.shape(1));
-    int ifmap_w = ifmap.shape(2);
-    int ifmap_h = ifmap.shape(1);
-
-    int kernel = weights.shape(3);
-    assert(ifmap_w >= kernel);
-    assert(ifmap_h >= kernel);
-
-    int ofmap_w = ifmap_w - (kernel - 1);
-    int ofmap_h = ifmap_h - (kernel - 1);
-    int ofmap_c = weights.shape(0);
-
-    xt::xarray<int> ofmap = xt::arange(ofmap_c * ofmap_w * ofmap_h).reshape({ofmap_c, ofmap_h, ofmap_w});
-
-    // conv2d stride 1
-    for (auto f = 0; f < ofmap_c; f++)
+    template <typename DataType>
+    xt::xarray<int> generate_weights(int filter_out_dim, int channel_in_dim, int kernel)
     {
-        auto weight_tensor_view = xt::view(weights, f, xt::all(), xt::all(), xt::all());
-        xt::xarray<int> flatten_weight(xt::flatten(weight_tensor_view));
-        for (auto h = 0; h < ofmap_h; h++)
+        int kernel_size = kernel * kernel;
+        xt::xarray<int> weights = xt::arange(1, channel_in_dim * filter_out_dim * kernel_size + 1);
+
+        weights.reshape({filter_out_dim, channel_in_dim, kernel, kernel});
+        return weights;
+    }
+
+    template <typename DataType>
+    xt::xarray<int> pad_weights(Arch<DataType> &arch, xt::xarray<int> weights, int filter_out_dim, int channel_in_dim, int kernel, UnrollOrientation unroll_orientation)
+    {
+        int kernel_size = kernel * kernel;
+
+        vector<vector<deque<int>>> pe_weights(arch.filter_count, vector<deque<int>>(arch.channel_count, deque<int>()));
+
+        long unsigned int verticle_padding;
+        long unsigned int horizontal_padding;
+
+        switch (unroll_orientation)
         {
-            for (auto w = 0; w < ofmap_w; w++)
+        case UnrollOrientation::HORIZONTAL:
+        {
+            weights.reshape({filter_out_dim, channel_in_dim * kernel_size});
+            verticle_padding = ceil((float)filter_out_dim / arch.filter_count) * arch.filter_count - filter_out_dim;
+            horizontal_padding = ceil((float)(channel_in_dim * kernel_size) / arch.channel_count) * arch.channel_count - (channel_in_dim * kernel_size);
+            break;
+        }
+        default:
+            cout << "INVALID ORIENTATION" << endl;
+            exit(EXIT_FAILURE);
+            break;
+        }
+
+        xt::xarray<int> padded_weights = xt::pad(weights, {{0, verticle_padding}, {0, horizontal_padding}}, xt::pad_mode::constant, PAD);
+
+        return padded_weights;
+    }
+
+    bool validate_output_1x1(xt::xarray<int> ifmap, xt::xarray<int> weights, xt::xarray<int> arch_output)
+    {
+        // weights.shape() = F*C*K*K
+        assert(weights.shape().size() == 4);
+        // cout << xt::adapt(weights.shape()) << endl;
+        // ifmap.shape() = C*H*W
+        assert(ifmap.shape().size() == 3);
+        // cout << xt::adapt(ifmap.shape()) << endl;
+        // cout << ifmap << endl;
+        // symmetric kernel
+        assert(weights.shape(3) == weights.shape(2));
+
+        // ifmap channel = weights channel in
+        assert(ifmap.shape(0) == weights.shape(1));
+        int ifmap_w = ifmap.shape(2);
+        int ifmap_h = ifmap.shape(1);
+
+        int kernel = weights.shape(3);
+        assert(ifmap_w >= kernel);
+        assert(ifmap_h >= kernel);
+
+        int ofmap_w = ifmap_w - (kernel - 1);
+        int ofmap_h = ifmap_h - (kernel - 1);
+        int ofmap_c = weights.shape(0);
+
+        xt::xarray<int> ofmap = xt::arange(ofmap_c * ofmap_w * ofmap_h).reshape({ofmap_c, ofmap_h, ofmap_w});
+
+        // conv2d stride 1
+        for (auto f = 0; f < ofmap_c; f++)
+        {
+            auto weight_tensor_view = xt::view(weights, f, xt::all(), xt::all(), xt::all());
+            xt::xarray<int> flatten_weight(xt::flatten(weight_tensor_view));
+            for (auto h = 0; h < ofmap_h; h++)
             {
-                auto ifmap_tensor_view = xt::view(ifmap, xt::all(), xt::range(h, h + kernel), xt::range(w, w + kernel));
-                xt::xarray<int> flattened_ifmap(xt::flatten(ifmap_tensor_view));
-                auto val = xt::linalg::dot(flattened_ifmap, flatten_weight);
-                ofmap(f, h, w) = val(0);
+                for (auto w = 0; w < ofmap_w; w++)
+                {
+                    auto ifmap_tensor_view = xt::view(ifmap, xt::all(), xt::range(h, h + kernel), xt::range(w, w + kernel));
+                    xt::xarray<int> flattened_ifmap(xt::flatten(ifmap_tensor_view));
+                    auto val = xt::linalg::dot(flattened_ifmap, flatten_weight);
+                    ofmap(f, h, w) = val(0);
+                }
             }
         }
+
+        return ofmap == arch_output;
     }
 
-    return ofmap == arch_output;
-}
-
-template xt::xarray<int> generate_weights<sc_int<32>>(int filter_out_dim, int channel_in_dim, int kernel);
-template xt::xarray<int> generate_ifmap<sc_int<32>>(Arch<sc_int<32>> &arch, int channel_in, int ifmap_h, int ifmap_w);
-template xt::xarray<int> pad_weights<sc_int<32>>(Arch<sc_int<32>> &arch, xt::xarray<int> weights, int filter_out_dim, int channel_in_dim, int kernel, UnrollOrientation unroll_orientation);
+    template xt::xarray<int> generate_weights<sc_int<32>>(int filter_out_dim, int channel_in_dim, int kernel);
+    template xt::xarray<int> generate_ifmap<sc_int<32>>(Arch<sc_int<32>> &arch, int channel_in, int ifmap_h, int ifmap_w);
+    template xt::xarray<int> pad_weights<sc_int<32>>(Arch<sc_int<32>> &arch, xt::xarray<int> weights, int filter_out_dim, int channel_in_dim, int kernel, UnrollOrientation unroll_orientation);
 
 }
--- tests/estimation_enviornment.cc
+++ tests/estimation_enviornment.cc
@@ -2,6 +2,7 @@
 #define __ESTIMATION_ENVIORNMENT_CC
 
 #include <systemc.h>
+#include "descriptor_compiler.hh"
 #include "hero.hh"
 #include "layer_generation.hh"
 #include "GlobalControl.hh"
@@ -88,160 +89,6 @@ xt::xarray<int> dram_store(Arch<DataType> &arch, int filter_out, int ofmap_h, in
 }
 
 template <typename DataType>
-void generate_and_load_pe_program(Arch<DataType> &arch, int ifmap_h, int ifmap_w)
-{
-    int stream_size = ifmap_h * ifmap_w;
-    int delay_offset = 1;
-    for (int channel_column = 0; channel_column < arch.channel_count; channel_column++)
-    {
-        for (int filter_row = 0; filter_row < arch.filter_count; filter_row++)
-        {
-            PE<DataType> &cur_pe = arch.pe_array[filter_row * arch.channel_count + channel_column];
-            vector<Descriptor_2D> program;
-            program.push_back(Descriptor_2D::delay_inst(channel_column + delay_offset));
-            program.push_back(Descriptor_2D::genhold_inst(0, stream_size, cur_pe.weights.size() - 1, 1));
-            program.push_back(Descriptor_2D::suspend_inst());
-            cur_pe.loadProgram(program);
-        }
-    }
-}
-
-template <typename DataType>
-void generate_and_load_psum_program(Arch<DataType> &arch, xt::xarray<int> padded_weights, int ofmap_h, int ofmap_w)
-{
-    int verticle_tile_count = padded_weights.shape()[0] / arch.filter_count;
-    int horizontal_tile_count = padded_weights.shape()[1] / arch.channel_count;
-
-    int stream_size = ofmap_h * ofmap_w;
-
-    xt::xarray<int> run_bitmap = xt::zeros<int>({verticle_tile_count, (int)arch.filter_count});
-    for (auto filter_offset = 0; filter_offset < (int)padded_weights.shape()[0]; filter_offset += arch.filter_count)
-    {
-        for (auto channel_offset = 0; channel_offset < (int)padded_weights.shape()[1]; channel_offset += arch.channel_count)
-        {
-            auto tiled_view = xt::view(padded_weights, xt::range(filter_offset, filter_offset + arch.filter_count), xt::range(channel_offset, channel_offset + arch.channel_count));
-            for (int filter = 0; filter < arch.filter_count; filter++)
-            {
-                int verticle_tile_idx = filter_offset / arch.filter_count;
-                if (tiled_view(filter, 0) != -1)
-                {
-                    run_bitmap(verticle_tile_idx, filter) = 1;
-                }
-            }
-        }
-    }
-
-    // cout << padded_weights << endl;
-    // cout << run_bitmap << endl;
-
-    for (int write_gen_idx = 0; write_gen_idx < arch.filter_count; write_gen_idx++)
-    {
-        vector<Descriptor_2D> program;
-
-        program.push_back(Descriptor_2D::delay_inst(arch.channel_count + 1));
-        for (int v = 0; v < verticle_tile_count; v++)
-        {
-            auto active = run_bitmap(v, write_gen_idx);
-            if (active)
-            {
-                for (int h = 0; h < horizontal_tile_count; h++)
-                {
-                    program.push_back(Descriptor_2D::stream_inst(v * arch.filter_count * stream_size + write_gen_idx * stream_size, stream_size - 1, 0));
-                }
-            }
-        }
-
-        program.push_back(Descriptor_2D::suspend_inst());
-        Descriptor_2D::make_sequential(program);
-        arch.psum_mem.generators.at(write_gen_idx).loadProgram(program);
-    }
-
-    for (int read_gen_idx = arch.filter_count; read_gen_idx < arch.filter_count * 2; read_gen_idx++)
-    {
-        vector<Descriptor_2D> program;
-        program.push_back(Descriptor_2D::delay_inst(3));
-
-        for (int v = 0; v < verticle_tile_count; v++)
-        {
-            auto active = run_bitmap(v, (read_gen_idx - arch.filter_count));
-            if (active)
-            {
-                program.push_back(Descriptor_2D::delay_inst(stream_size - 4 * (v == 0) - 1));
-                for (int h = 1; h < horizontal_tile_count; h++)
-                {
-                    program.push_back(Descriptor_2D::stream_inst((v * arch.filter_count * stream_size) + (read_gen_idx - arch.filter_count) * stream_size, stream_size - 1, 0));
-                }
-            }
-        }
-        program.push_back(Descriptor_2D::suspend_inst());
-        Descriptor_2D::make_sequential(program);
-        arch.psum_mem.generators.at(read_gen_idx).loadProgram(program);
-    }
-}
-
-template <typename DataType>
-void generate_and_load_ifmap_in_program(Arch<DataType> &arch, xt::xarray<int> padded_weights, int ifmap_h, int ifmap_w)
-{
-    int verticle_tile_count = padded_weights.shape()[0] / arch.filter_count;
-    int horizontal_tile_count = padded_weights.shape()[1] / arch.channel_count;
-
-    xt::xarray<int> run_bitmap = xt::zeros<int>({verticle_tile_count, horizontal_tile_count, (int)arch.channel_count});
-    for (auto filter_offset = 0; filter_offset < (int)padded_weights.shape()[0]; filter_offset += arch.filter_count)
-    {
-        for (auto channel_offset = 0; channel_offset < (int)padded_weights.shape()[1]; channel_offset += arch.channel_count)
-        {
-            auto tiled_view = xt::view(padded_weights, xt::range(filter_offset, filter_offset + arch.filter_count), xt::range(channel_offset, channel_offset + arch.channel_count));
-            for (int channel = 0; channel < arch.channel_count; channel++)
-            {
-                int verticle_tile_idx = filter_offset / arch.filter_count;
-                int horizontal_tile_idx = channel_offset / arch.channel_count;
-                if (tiled_view(0, channel) != -1)
-                {
-                    run_bitmap(verticle_tile_idx, horizontal_tile_idx, channel) = 1;
-                }
-            }
-        }
-    }
-
-    // cout << padded_weights << endl;
-
-    // cout << run_bitmap << endl;
-
-    int ag_idx = 0;
-    for (auto &ag : arch.ifmap_mem.generators)
-    {
-        std::deque<Descriptor_2D> program;
-        auto systolic_delay = Descriptor_2D::delay_inst(ag_idx);
-        program.push_back(systolic_delay);
-        for (int v = 0; v < verticle_tile_count; v++)
-        {
-            for (int h = 0; h < horizontal_tile_count; h++)
-            {
-                int active = run_bitmap(v, h, ag_idx);
-                int stream_size = ifmap_h * ifmap_w;
-                int stream_start_idx = h * arch.channel_count * stream_size + ag_idx * stream_size;
-
-                if (active)
-                {
-                    auto stream_inst = Descriptor_2D::stream_inst(stream_start_idx, stream_size - 1, 0);
-                    program.push_back(stream_inst);
-                }
-                else
-                {
-                    auto delay_inst = Descriptor_2D::delay_inst(stream_size - 1);
-                    program.push_back(delay_inst);
-                }
-            }
-        }
-        program.push_back(Descriptor_2D::suspend_inst());
-        vector<Descriptor_2D> prog_vec(program.begin(), program.end());
-        Descriptor_2D::make_sequential(prog_vec);
-        ag.loadProgram(prog_vec);
-        ag_idx++;
-    }
-}
-
-template <typename DataType>
 void load_padded_weights_into_pes(Arch<DataType> &arch, xt::xarray<int> padded_weights)
 {
     vector<vector<deque<int>>> pe_weights(arch.filter_count, vector<deque<int>>(arch.channel_count, deque<int>()));
@@ -314,9 +161,9 @@ void sim_and_get_results(int ifmap_h, int ifmap_w, int k, int c_in, int f_out, i
     // cout << "PADDED WEIGHTS" << endl;
     // cout << padded_weights << endl;
 
-    generate_and_load_pe_program(arch, ifmap_h, ifmap_w);
-    generate_and_load_ifmap_in_program(arch, padded_weights, ifmap_h, ifmap_w);
-    generate_and_load_psum_program(arch, padded_weights, ofmap_h, ofmap_w);
+    GenerateDescriptors1x1::generate_and_load_pe_program(arch, ifmap_h, ifmap_w);
+    GenerateDescriptors1x1::generate_and_load_ifmap_in_program(arch, padded_weights, ifmap_h, ifmap_w);
+    GenerateDescriptors1x1::generate_and_load_psum_program(arch, padded_weights, ofmap_h, ofmap_w);
 
     control.set_program(true);
     sc_start(1, SC_NS);
