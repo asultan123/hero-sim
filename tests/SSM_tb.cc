26dded0c76d7b2ec146be9feacff0c3a5885f5cd
asultan123 <asultan@aucegypt.edu>
2022-03-09 01:35:09 -0500

    Initial verification start

--- tests/SSM_tb.cc
+++ tests/SSM_tb.cc
@@ -1,8 +1,12 @@
 #include "SSM.hh"
+#include <fmt/format.h>
+#include <random>
 #include <systemc.h>
 // #define DEBUG
 using std::cout;
 using std::endl;
+
+#define SEED 1234
 template <typename DataType> struct SSM_TB : public sc_module
 {
     const unsigned int input_size = 16;
@@ -15,17 +19,46 @@ template <typename DataType> struct SSM_TB : public sc_module
           control("global_control_channel", sc_time(1, SC_NS), tf), dut("dut", control, input_size, output_size, tf)
     {
         tf->set_time_unit(1, SC_PS);
-        cout << "Instantiated SSM TB with name " << this->name() << endl;
+        fmt::print("Instantiated SSM TB with name {}\n", this->name());
     }
-    bool validate_reset()
+    bool validate_multi_in_single_out()
     {
-        sc_start(10, SC_NS);
+        control.set_reset(true);
+        control.set_program(false);
+        sc_start(2, SC_NS);
+        control.set_reset(false);
+        control.set_program(true);
+
+        std::default_random_engine eng{SEED};
+        std::uniform_real_distribution<float> inst_dist(16, 32);
+        auto inst_count = static_cast<int>(inst_dist(eng));
+
+        std::uniform_real_distribution<float> port_dist(0, 16);
+        std::uniform_real_distribution<float> wait_dist(16, 128);
+
+        std::vector<Descriptor_2D> mux_program;
+        for (int inst = 0; inst < inst_count; inst++)
+        {
+            auto selected_port = static_cast<int>(port_dist(eng));
+            auto wait_time = static_cast<int>(wait_dist(eng));
+            mux_program.push_back(Descriptor_2D::delay_inst(selected_port, wait_time));
+        }
+
+        Descriptor_2D::make_sequential(mux_program);
+
+        dut.load_in_port_program(mux_program);
+
+        sc_start(1, SC_NS);
+        control.set_reset(false);
+        control.set_program(false);
+        control.set_enable(true);
+
         return true;
     }
     int run_tb()
     {
         cout << "Validating Reset" << endl;
-        if (!validate_reset())
+        if (!validate_multi_in_single_out())
         {
             cout << "Reset Failed" << endl;
             return -1;
