d56709da8b1fcfbebb9a51aa540dfcac618e2da9
asultan123 <asultan@aucegypt.edu>
2022-05-13 13:02:00 -0400

    did quick sweep of testcases in entire network library

--- scripts/frontend.py
+++ scripts/frontend.py
@@ -830,14 +830,14 @@ if __name__ == "__main__":
         "filter_count": 32,
         "channel_count": 18,
         "directly_supported_kernels": [(1, 1), (3, 3)],
-        "ifmap_mem_ub": 2**20,
+        "ifmap_mem_ub": 2**20 // 18 * 18,
         "allow_ifmap_distribution": True,
-        "ofmap_mem_ub": 2**21,
+        "ofmap_mem_ub": 2**20,
         "allow_ofmap_distribution": True,
     }
-    for model_name, model in models:
-        RESULTS_CSV_PATH = f"../data/{model_name}.csv"
-        eval_network(model, arch_config, model_name=model_name, processed_network=True)
+    # for model_name, model in models:
+    #     RESULTS_CSV_PATH = f"../data/{model_name}.csv"
+    #     eval_network(model, arch_config, model_name=model_name, processed_network=True)
 
     # arch_config = {
     #     "filter_count": 32,
@@ -856,11 +856,13 @@ if __name__ == "__main__":
     #     test_case_list[2000:], layer_name_tracker
     # )
 
-    # layer_name_tracker = {}
-    # for model_name, layer_dims in tqdm(layer_dims_generator()):
-    #     test_case_list, layer_name_tracker = convert_collected_model_layers_to_testcases(
-    #         layer_dims, arch_config, model_name, layer_name_tracker
-    #     )
+    layer_name_tracker = {}
+    for model_name, layer_dims in tqdm(layer_dims_generator()):
+        test_case_list, layer_name_tracker = convert_collected_model_layers_to_testcases(
+            layer_dims, arch_config, model_name, layer_name_tracker
+        )
+    with open("../data/timm_lib_testcases.pickle", "wb") as file:
+        pickle.dump(layer_name_tracker, file) 
 
     # #     total_test_cases_count += len(cases)
     # print(len(test_case_list))
