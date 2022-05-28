f54a6296643c34749ffb12f84c43daebd650af9d
asultan123 <asultan@aucegypt.edu>
2022-05-27 23:47:32 -0400

    fixed timm eval script  out csv path

--- scripts/eval_all_timm_networks.py
+++ scripts/eval_all_timm_networks.py
@@ -1,4 +1,7 @@
 from frontend import *
+import config
+import frontend as front
+from timeit import default_timer as timer
 
 arch_config = {
     "filter_count": 32,
@@ -10,11 +13,15 @@ arch_config = {
     "allow_ofmap_distribution": True,
 }
 
-RESULTS_CSV_PATH = "../data/timm_0_5314.csv"
+front.config.RESULTS_CSV_PATH = "../data/timm.csv"
 
 with open("../data/timm_lib_testcases.pickle", "rb") as file:
     layer_name_tracker = pickle.load(file)
 
 test_case_list = list(layer_name_tracker.keys())
 test_case_list = sorted(test_case_list)
+
+start = timer()
 result_df = launch_workers_with_test_cases(test_case_list, layer_name_tracker)
+end = timer()
+print(end - start)
