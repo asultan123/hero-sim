7fb11e9e250b437d4101b5a3c0f5919e90d7b10e
asultan123 <asultan@aucegypt.edu>
2022-06-08 02:38:42 -0400

    moved layer dims generator to timm test case generator

--- scripts/timm_test_case_generator.py
+++ scripts/timm_test_case_generator.py
@@ -1,5 +1,27 @@
 from frontend import *
 
+def layer_dims_generator():
+    processed_models_files = os.listdir("../data/processed_models")
+    processed_models_names = [
+        "".join(filename.split(".")[:-2]) for filename in processed_models_files
+    ]
+    processed_models_filepaths = [
+        os.path.join("../data/processed_models", filename)
+        for filename in os.listdir("../data/processed_models")
+    ]
+
+    ignore_list = pickle.load(open("../data/frontend_ignore_list.pickle", "rb"))
+
+    for model_name, path in tqdm(
+        list(zip(processed_models_names, processed_models_filepaths))
+    ):
+        if model_name in ignore_list:
+            continue
+
+        with open(path, "rb") as file:
+            layer_dims = pickle.load(file)
+        yield model_name, layer_dims
+        
 arch_config = {
     "filter_count": 32,
     "channel_count": 18,
@@ -20,5 +42,5 @@ for model_name, layer_dims in tqdm(layer_dims_generator()):
     )
     
     
-with open("../data/timm_lib_testcases.pickle", "wb") as file:
+with open("../data/timm_lib_testcases_1.pickle", "wb") as file:
     pickle.dump(layer_name_tracker, file)
